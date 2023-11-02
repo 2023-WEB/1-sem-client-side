2f4deccd8d8771de55b035a5b1f54c72c6f40254
Mathias Nielsen <97943929+cph-mtnl@users.noreply.github.com>
2023-11-02 10:38:57 +0100

    fixed typo

--- business-card/src/components/molecules/SmallCard.jsx
+++ business-card/src/components/molecules/SmallCard.jsx
@@ -3,7 +3,7 @@ import styles from "./SmallCard.module.css";
 
 export default function SmallCard({ name, job, website }) {
   return (
-    <div className={styles.ca}>
+    <div className={styles.card}>
       <p>{name}</p>
       <p>{job}</p>
       <p>{website}</p>
