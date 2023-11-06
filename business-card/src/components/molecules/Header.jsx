8c3acfefdeb4ec5764eaa5324d39b2b2bc7284a5
Mathias Nielsen <97943929+cph-mtnl@users.noreply.github.com>
2023-11-06 10:35:29 +0100

    added 3 controlled inputs, for creating a new business card

--- business-card/src/components/molecules/Header.jsx
+++ business-card/src/components/molecules/Header.jsx
@@ -4,6 +4,9 @@ import Drawer from "react-modern-drawer";
 export default function Header() {
   // All of the state
   const [isOpen, setIsOpen] = useState(false);
+  const [name, setName] = useState("");
+  const [job, setJob] = useState("");
+  const [website, setWebsite] = useState("");
 
   // all of our side effects
 
@@ -17,12 +20,47 @@ export default function Header() {
     setIsOpen(false);
   }
 
+  function handleNameChange(event) {
+    setName(event.target.value);
+  }
+
+  function handleJobChange(event) {
+    setJob(event.target.value);
+  }
+
+  function handleWebsiteChange(event) {
+    setWebsite(event.target.value);
+  }
+
   return (
     <nav>
       <h1>Business Cards</h1>
       <button onClick={handleClick}>Create new</button>
       <Drawer open={isOpen} onClose={handleClose} direction="right">
-        <div>Hello World</div>
+        // Create a new business Card, // Name, Job, Website
+        <form>
+          <input
+            type="text"
+            name="name"
+            placeholder="name"
+            value={name}
+            onChange={handleNameChange}
+          />
+          <input
+            type="text"
+            name="job"
+            placeholder="job title"
+            value={job}
+            onChange={handleJobChange}
+          />
+          <input
+            type="text"
+            name="website"
+            placeholder="website"
+            value={website}
+            onChange={handleWebsiteChange}
+          />
+        </form>
       </Drawer>
     </nav>
   );
