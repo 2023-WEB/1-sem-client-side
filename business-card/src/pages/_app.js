758786f145cabf1d85cee9068ea15d50673c9bf8
Mathias Nielsen <97943929+cph-mtnl@users.noreply.github.com>
2023-11-20 10:31:13 +0100

    useContext code

--- business-card/src/pages/_app.js
+++ business-card/src/pages/_app.js
@@ -1,11 +1,26 @@
 import "@/styles/globals.css";
 import "react-modern-drawer/dist/index.css";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
+import { MyContext } from "@/context/MyContext";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }) {
+  // All of the state
+  const [userInfo, setUserInfo] = useState("Mathias");
+  const [cardData, setCardData] = useState([]);
+
+  const contextValue = {
+    userInfo,
+    setUserInfo,
+    cardData,
+    setCardData,
+  };
+
   return (
-    <DefaultLayout>
-      <Component {...pageProps} />;
-    </DefaultLayout>
+    <MyContext.Provider value={contextValue}>
+      <DefaultLayout>
+        <Component {...pageProps} />;
+      </DefaultLayout>
+    </MyContext.Provider>
   );
 }
--- business-card/src/pages/booking/room/blabla.js
+++ business-card/src/pages/booking/room/blabla.js
@@ -1,5 +1,10 @@
 import React from "react";
+import useMyContext from "@/context/MyContext";
 
 export default function blabla() {
+  const value = useMyContext();
+
+  console.log(value.userInfo);
+
   return <div>blabla</div>;
 }
